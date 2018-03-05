bd6da9888a3246a200cdc5c6add492cb9fca3f26
Daniel Mason <danielboeker@iinet.net.au>
2018-03-05 16:48:41 +0800

    added runnerfile.sh

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
 source ./runner/src/runner.sh
-source_files=(bin/runner src/*.sh)
 
-task_hello() {
-    echo "Hello"
-}
 
-task_test() {
-    bash test/test.sh >/dev/null
-}
-
-task_readme() {
-    doctoc README.md
-}
-
-task_default() {
-    runner_parallel shellcheck test
+task_build_receive() {
+  docker build -t danobot/rfgateway ./receive
 }
